345d0bd4cc03e5582a1b3bd13f1487b55d78e809
amir15bfk <amir15bfk@gmail.com>
2021-11-24 17:53:20 +0000

    still work

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,6 +1,9 @@
+from discord import Intents
+from discord import Embed
 from discord.ext.commands import Bot as BotBase
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import os
+import random
 
 PREFIX = ">"
 OWNER_IDS=[403096560914923531]
@@ -11,7 +14,8 @@ class Bot(BotBase):
     self.guild = None
     self.scheduler = AsyncIOScheduler()
 
-    super().__init__(command_prefix=PREFIX, owner_ids=OWNER_IDS)
+    super().__init__(command_prefix=PREFIX, owner_ids=OWNER_IDS,
+    itents=Intents.all())
   
   def run(self,version):
     self.VERSION = version
@@ -25,17 +29,29 @@ class Bot(BotBase):
   
   async def on_disconnect(self):
     print("bot disconnected")
+    channel = self.get_channel(860464413777330206)
+    await channel.send("i'm down :small_red_triangle_down:  ")
 
   async def on_ready(self):
     if not self.ready:
       self.ready =True 
       self.guild = self.get_guild(800009861982191617)
       print("bot ready")
+      channel = self.get_channel(860464413777330206)
+      embed = Embed(title="Now Online",description ="the bot is live",color=0x00ff00)
+      embed.add_field(name ="version",value=self.VERSION,inline=True)
+
+      await channel.send(embed=embed)
     else :
       print("bot reconnected")
 
   async def on_message(self,message):
-    pass
+    if message.content.startswith('>IT') or message.content.startswith('>it'):
+      n = random.randint(3, 15)
+      emoje = [":fire:",":star2:",":sunglasses:" ,":loudspeaker:"]
+      title='C'*n+'!'+emoje[random.randrange(len(emoje))]
+      embed = Embed(title=title,color=0xff0000)
+      await message.channel.send(embed=embed)
 
 
 bot = Bot()
